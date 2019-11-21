4061139aac0866edee708d17984f03eb1c38f408
Sonata Katt <sonata.abdullh@gmail.com>
2019-11-21 12:00:30 -0500

    added html wrapper

--- client/src/App.js
+++ client/src/App.js
@@ -1,43 +1,55 @@
 import React, { useState, useEffect } from "react";
 
-// SERVICES
-import productService from './services/productService';
+// // SERVICES
+// import productService from './services/productService';
 
-function App() {
-  const [products, setproducts] = useState(null);
 
-  useEffect(() => {
-    if(!products) {
-      getProducts();
-    }
-  })
+var __html = require('./mini-launch.html');
+var template = { __html: __html };
 
-  const getProducts = async () => {
-    let res = await productService.getAll();
-    console.log(res);
-    setproducts(res);
-  }
-
-  const renderProduct = product => {
-    return (
-      <li key={product._id} className="list__item product">
-        <h3 className="product__name">{product.name}</h3>
-        <p className="product__description">{product.description}</p>
-      </li>
+React.module.exports = React.createClass({
+  render: function() {
+    return(
+      <div dangerouslySetInnerHTML={template} />
     );
-  };
+  }
+});
 
-  return (
-    <div className="App">
-      <ul className="list">
-        {(products && products.length > 0) ? (
-          products.map(product => renderProduct(product))
-        ) : (
-          <p>No products found</p>
-        )}
-      </ul>
-    </div>
-  );
-}
+// function App() {
+//   const [products, setproducts] = useState(null);
 
-export default App;
+//   useEffect(() => {
+//     if(!products) {
+//       getProducts();
+//     }
+//   })
+
+//   const getProducts = async () => {
+//     let res = await productService.getAll();
+//     console.log(res);
+//     setproducts(res);
+//   }
+
+//   const renderProduct = product => {
+//     return (
+//       <li key={product._id} className="list__item product">
+//         <h3 className="product__name">{product.name}</h3>
+//         <p className="product__description">{product.description}</p>
+//       </li>
+//     );
+//   };
+
+//   return (
+//     <div className="App">
+//       <ul className="list">
+//         {(products && products.length > 0) ? (
+//           products.map(product => renderProduct(product))
+//         ) : (
+//           <p>No products found</p>
+//         )}
+//       </ul>
+//     </div>
+//   );
+// }
+
+// export default App;
