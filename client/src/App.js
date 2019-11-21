587c12006090ec94c1dfc14791d6ebc5bbd14ea4
Sonata Katt <sonata.abdullh@gmail.com>
2019-11-21 17:03:24 -0500

    integrated fhir js

--- client/src/App.js
+++ client/src/App.js
@@ -1,55 +1,43 @@
 import React, { useState, useEffect } from "react";
 
-// // SERVICES
-// import productService from './services/productService';
+// SERVICES
+import productService from './services/productService';
 
+function App() {
+  const [products, setproducts] = useState(null);
 
-var __html = require('./mini-launch.html');
-var template = { __html: __html };
+  useEffect(() => {
+    if(!products) {
+      getProducts();
+    }
+  })
 
-React.module.exports = React.createClass({
-  render: function() {
-    return(
-      <div dangerouslySetInnerHTML={template} />
-    );
+  const getProducts = async () => {
+    let res = await productService.getAll();
+    console.log(res);
+    setproducts(res);
   }
-});
 
-// function App() {
-//   const [products, setproducts] = useState(null);
+  const renderProduct = product => {
+    return (
+      <li key={product._id} className="list__item product">
+        <h3 className="product__name">{product.name}</h3>
+        <p className="product__description">{product.description}</p>
+      </li>
+    );
+  };
 
-//   useEffect(() => {
-//     if(!products) {
-//       getProducts();
-//     }
-//   })
+  return (
+    <div className="App">
+      <ul className="list">
+        {(products && products.length > 0) ? (
+          products.map(product => renderProduct(product))
+        ) : (
+          <p>No products found</p>
+        )}
+      </ul>
+    </div>
+  );
+}
 
-//   const getProducts = async () => {
-//     let res = await productService.getAll();
-//     console.log(res);
-//     setproducts(res);
-//   }
-
-//   const renderProduct = product => {
-//     return (
-//       <li key={product._id} className="list__item product">
-//         <h3 className="product__name">{product.name}</h3>
-//         <p className="product__description">{product.description}</p>
-//       </li>
-//     );
-//   };
-
-//   return (
-//     <div className="App">
-//       <ul className="list">
-//         {(products && products.length > 0) ? (
-//           products.map(product => renderProduct(product))
-//         ) : (
-//           <p>No products found</p>
-//         )}
-//       </ul>
-//     </div>
-//   );
-// }
-
-// export default App;
+export default App;
--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
+const smart   = require("fhirclient");
+const session = require("express-session");
+
 // IMPORT MODELS
 require('./models/product');
 
@@ -12,21 +15,147 @@ mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost:27017/node-reac
 
 app.use(bodyParser.json());
 
+app.use(session({
+  secret: "my secret",
+  resave: false,
+  saveUninitialized: false
+}));
+
 //IMPORT ROUTES
 require('./routes/productRoute')(app);
 
+// The settings that we use to connect to our SMART on FHIR server
+const smartSettings = {
+  clientId   : "my-client-id",
+  redirectUri: "/app",
+  scope      : "launch/patient patient/*.read openid fhirUser",
+  iss        : "https://launch.smarthealthit.org/v/r2/sim/eyJrIjoiMSIsImIiOiJzbWFydC03Nzc3NzA1In0/fhir"
+};
+
+// Just a simple function to reply back with some data (the current patient if
+// we know who he is, or all patients otherwise
+async function handler(client, res) {
+  const data = await (
+      client.patient.id ? client.patient.read() : client.request("Patient")
+  );
+  res.type("json").send(JSON.stringify(data, null, 4));
+  // res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+}
+
+// authorizes my app
+app.get("/launch", (req, res, next) => {
+  smart(req, res).authorize(smartSettings).catch(next);
+});
+
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
-  
-    const path = require('path');
-    app.get('*', (req,res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    })
-  
-  }
+
+  app.use(express.static('client/build'));
+
+  const path = require('path');
+  // app.get('*', (req,res) => {
+  //     //smart(req, res).ready().then(client => handler(client, res));
+  //     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  // })
+
+  app.get("/app", (req, res) => {
+    smart(req, res).ready().then(client => handler(client, res));
+  });
+
+}
   
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`app running on port ${PORT}`)
 });
+
+/////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////
+
+
+// const smart   = require("fhirclient");
+// const session = require("express-session");
+// const app     = require("express")();
+
+
+// // The SMART state is stored in a session. If you want to clear your session
+// // and start over, you will have to delete your "connect.sid" cookie!
+// app.use(session({
+//     secret: "my secret",
+//     resave: false,
+//     saveUninitialized: false
+// }));
+
+// // The settings that we use to connect to our SMART on FHIR server
+// const smartSettings = {
+//     clientId   : "my-client-id",
+//     redirectUri: "/app",
+//     scope      : "launch/patient patient/*.read openid fhirUser",
+//     iss        : "https://launch.smarthealthit.org/v/r2/sim/eyJrIjoiMSIsImIiOiJzbWFydC03Nzc3NzA1In0/fhir"
+// };
+
+// // Just a simple function to reply back with some data (the current patient if
+// // we know who he is, or all patients otherwise
+// async function handler(client, res) {
+//     const data = await (
+//         client.patient.id ? client.patient.read() : client.request("Patient")
+//     );
+//     res.type("json").send(JSON.stringify(data, null, 4));
+// } 
+
+// // =============================================================================
+// // LAUNCHING
+// // =============================================================================
+// // 1. If you remove the iss options and request https://c0che.sse.codesandbox.io/launch
+// //    it will throw because the FHIR service url is not known!
+// // 2. If an EHR calls it, it will append "launch" and "state" parameters. Try
+// //    it by loading https://c0che.sse.codesandbox.io/launch?iss=https://launch.smarthealthit.org/v/r3/fhir&launch=eyJhIjoiMSIsImciOiIxIn0
+// // 3. If you only add an "iss" url parameter (no "launch"), you are doing a
+// //    "dynamic" standalone launch. The app cannot obtain a launch context but
+// //    it is still useful to be able to do that. In addition, the SMART Sandbox
+// //    can be used to build a standalone launch url that contains embedded launch
+// //    context which may be perfect for previewing you app. For example load this
+// //    to launch the app with Angela Montgomery from DSTU-2:
+// //    https://c0che.sse.codesandbox.io/launch?iss=https://launch.smarthealthit.org/v/r2/sim/eyJrIjoiMSIsImIiOiJzbWFydC03Nzc3NzA1In0/fhir
+// // 4. We have an "iss" authorize option to make this do standalone launch by
+// //    default. In this case https://c0che.sse.codesandbox.io/launch will
+// //    not throw. Note that the "iss" url parameter takes precedence over the iss
+// //    option, so the app will still be launch-able from an EHR.
+// // 5. If an open server is passed as an "iss" option, or as "iss" url parameter,
+// //    no authorization attempt will be made and we will be redirected to the
+// //    redirect_uri (in this case we don't have launch context and there is no
+// //    selected patient so we show all patients instead). Try it:
+// //    https://c0che.sse.codesandbox.io/launch?iss=https://r3.smarthealthit.org
+// // 6. Finally, a "fhirServiceUrl" parameter can be passed as option or as url
+// //    parameter. It is like "iss" but will bypass the authorization (only useful
+// //    in testing and development). Example:
+// //    https://c0che.sse.codesandbox.io/launch?fhirServiceUrl=https://launch.smarthealthit.org/v/r3/fhir
+// app.get("/launch", (req, res, next) => {
+//     smart(req, res).authorize(smartSettings).catch(next);
+// });
+
+// // =============================================================================
+// // APP
+// // =============================================================================
+// // The app lives at your redirect_uri (in this case that is
+// // "https://c0che.sse.codesandbox.io/app"). After waiting for "ready()", you get
+// // a client instance that can be used to query the fhir server.
+// app.get("/app", (req, res) => {
+//     smart(req, res).ready().then(client => handler(client, res));
+// });
+
+// // =============================================================================
+// // SINGLE ROUTE
+// // =============================================================================
+// // In case you prefer to handle everything in one place, you can use the "init"
+// // method instead of "authorize" and then "ready". It takes the same options as
+// // "authorize"
+// app.get("/", (req, res) => {
+//     smart(req, res)
+//         .init({ ...smartSettings, redirectUri: "/" })
+//         .then(client => handler(client, res));
+// });
+
+
+// app.listen(8080);
